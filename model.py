7185b083c98a215a2a6e899ff14b2f90228dc1e7
KingMurky <staryunleegh@naver.com>
2022-02-10 01:43:50 +0900

    [0210] sublayer implementation clear!

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 class EmbeddingLayer(nn.Module):
     def __init__(self, num_embeddings, embedding_dim, padding_idx, layer_mode='embed') -> None:
@@ -35,24 +36,166 @@ class EmbeddingLayer(nn.Module):
             return output
             
 
-# class PositionalEncodingLayer(nn.modules):
-#     pass
+class PositionalEncodingLayer(nn.modules):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, n_position, model_dim):
+        position_encoding = [[position / np.power(10000, 2*i/model_dim) for i in range(model_dim)] for position in range(n_position)]
+        position_encoding = np.array(position_encoding)
+
+        position_encoding[:, 0::2] = np.sin(position_encoding[:, 0::2])
+        position_encoding[:, 1::2] = np.cos(position_encoding[:, 1::2])
+
+        position_encoding = torch.FloatTensor(position_encoding)
+
+        if torch.cuda.is_available():
+            position_encoding = position_encoding.to('cuda')
+
+        return position_encoding
 
 
-# class Encoder(nn.Module):
-#     pass
+class MultiHeadAttentionLayer(nn.Module):
+    def __init__(self, model_dim, q_dim, k_dim, v_dim, n_heads):
+        super().__init__()
+        self.model_dim = model_dim
+        self.q_dim = q_dim
+        self.k_dim = k_dim
+        self.v_dim = v_dim
+        self.n_heads = n_heads
+
+        self.wq = nn.Linear(model_dim, n_heads * q_dim, bias=False)
+        self.wk = nn.Linear(model_dim, n_heads * k_dim, bias=False)
+        self.wv = nn.Linear(model_dim, n_heads * v_dim, bias=False)
+        self.wo = nn.Linear(n_heads * v_dim, model_dim, bias=False)
+
+        self.ScaledDotProductAttention = ScaledDotProductAttentionLayer(k_dim)
+
+    def forward(self, q, k, v):
+
+        batch_size, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+
+        q = self.wq(q).view(batch_size, len_q, self.n_heads, self.q_dim)
+        k = self.wk(k).view(batch_size, len_k, self.n_heads, self.k_dim)
+        v = self.wv(v).view(batch_size, len_v, self.n_heads, self.v_dim)
+
+        q = torch.transpose(q, 1, 2)
+        k = torch.transpose(k, 1, 2)
+        v = torch.transpose(v, 1, 2)
+
+        attention = self.ScaledDotProductAttention(q, k, v)
+
+        attention = torch.transpose(attention, 1, 2)
+        attention = attention.view(batch_size, len_q, -1)
+
+        out = self.wo(attention)
+
+        return out
+
+        
+
+class PositionWiseFeedForwardLayer(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim):
+        super().__init__()
+
+        self.w1 = nn.Linear(in_dim, hid_dim)
+        self.w2 = nn.Linear(hid_dim, out_dim)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        out = self.w2(self.relu(self.w1(x)))
+
+        return out
 
 
-# class Decoder(nn.Module):
-#     pass
+class ScaledDotProductAttentionLayer(nn.Module):
+    def __init__(self, k_dim, mask=False):
+        super().__init__()
+        self.k_dim = k_dim
+        self.mask = mask
+    
+    def forward(self, q, k, v):
+        attention = torch.matmul(q, torch.transpose(k, 2, 3))
+        attention = attention / torch.sqrt(self.k_dim)
+
+        if self.mask is True:
+            attention.masked_fill(self.mask, 1e-9)
+
+        attention = torch.softmax(attention, -1)
+        attention = torch.matmul(attention, v)
+
+        return attention
 
 
-# class MultiHeadAttentionLayer(nn.Module):
-#     pass
+
+class EncoderLayer(nn.Module):
+    def __init__(self, model_dim, q_dim, k_dim, v_dim, n_heads, in_dim, hid_dim, out_dim):
+        super().__init__()
+
+        self.dropout = nn.Dropout(p=0.1)
+        self.layer_norm = nn.LayerNorm(model_dim, eps=1e-6)
+
+        self.MultiHeadAttention = MultiHeadAttentionLayer(model_dim, q_dim, k_dim, v_dim, n_heads)
+        self.PositionWiseFeedForward = PositionWiseFeedForwardLayer(in_dim, hid_dim, out_dim)
 
 
-# class FeedForwardLayer(nn.Module):
-#     pass
+    def forward(self, x):
+        residual = x
+
+        q = k = v = x
+        attention = self.MultiHeadAttention(q, k, v)
+
+        out = self.dropout(attention)
+        out += residual
+        out = self.layer_norm(out)
+
+        residual = out
+        out = self.PositionWiseFeedForward(out)
+
+        out = self.dropout(out)
+        out += residual
+        out = self.layer_norm(out)
+
+        return out
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, model_dim, q_dim, k_dim, v_dim, n_heads, in_dim, hid_dim, out_dim):
+        super().__init__()
+        self.dropout = nn.Dropout(p=0.1)
+        self.layer_norm = nn.LayerNorm(model_dim, eps=1e-6)
+
+        self.MultiHeadAttention = MultiHeadAttentionLayer(model_dim, q_dim, k_dim, v_dim, n_heads)
+        self.MaskedMultiHeadAttention = MultiHeadAttentionLayer(model_dim, q_dim, k_dim, v_dim, n_heads, mask=True)
+        self.PositionWiseFeedForward = PositionWiseFeedForwardLayer(in_dim, hid_dim, out_dim)
+    
+    def forward(self, decoder_input, encoder_output):
+        residual = decoder_input
+
+        q = k = v = decoder_input
+        attention = self.MaskedMultiHeadAttention(q, k, v)
+
+        v = self.dropout(attention)
+        v += residual
+        v = self.layer_norm(v)
+
+        residual = v
+
+        q, k = encoder_output, encoder_output
+        attention = self.MultiHeadAttention(q, k, v)
+
+        out = self.dropout(attention)
+        out += residual
+        out = self.layer_norm(out)
+
+        residual = out
+
+        out = self.PositionWiseFeedForward(out)
+
+        out = self.dropout(out)
+        out += residual
+        out = self.layer_norm(out)
+
 
 
 
